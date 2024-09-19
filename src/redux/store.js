3e41f4473bb1be18fb8e2cd7f28ff4b44969c2d1
santhiyakm0 <santhiyakm04@gmail.com>
2024-09-19 12:46:06 +0530

    First Commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import  counterSlice  from './Slices/counter'
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    counter:counterSlice
+  },
 })
