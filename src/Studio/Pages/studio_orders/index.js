6a87cf42216fe3b849f9f37b5600efe2ab61964a
Subhash-Mani <subhashs192001@gmail.com>
2024-11-04 16:04:01 +0530

    api changes completed

--- src/Studio/Pages/studio_orders/index.js
+++ src/Studio/Pages/studio_orders/index.js
@@ -49,7 +49,7 @@ export const Studio_Order = () => {
         axios.put("https://subhashs.pythonanywhere.com/edit",formData).then((e) => {
 
             // console.log(e)
-            // alert("upload profile sucessfully")
+            alert("upload profile sucessfully")
             
         })
     }
@@ -64,22 +64,28 @@ export const Studio_Order = () => {
     //     navigate(`/clientdetails/${idvalue}`)
     // }
 
-    // const orderlist = () => {
-    //     axios.get(`https://agaram.academy/api/action.php?request=studio_getBookingDetails&studio_id=${logup.data.id}`).then((n) => {
-    //         // axios.get("https://agaram.academy/api/action.php?request=studio_getBookingDetails").then((n) => {
-    //             // axios.get("https://subhashs.pythonanywhere.com/users").then((n) => {
+    const orderlist = () => {
+        // axios.get(`https://agaram.academy/api/action.php?request=studio_getBookingDetails&studio_id=${logup.data.id}`).then((n) => {
+            // axios.get("https://agaram.academy/api/action.php?request=studio_getBookingDetails").then((n) => {
+                axios.get("https://subhashs.pythonanywhere.com/bookinglist").then((n) => {
 
-    //        console.log(n.data.data)
-    //         setorder(n.data.data)
-    //         setsearch(n.data.data)
+           console.log(n.data.data)
+            setorder(n.data)
+            setsearch(n.data)
             
-    //     })
-    // }
-    // useEffect(() => {
-    //     orderlist()
-    // },[])
+        })
+    }
+    useEffect(() => {
+        orderlist()
+    },[])
 
-    
+    const Deletedlist=(idvalue)=>{
+
+        axios.delete(`https://SanthiyaKumarMallika.pythonanywhere.com/deleted/${idvalue}`).then((value)=>{
+            console.log(value)
+            orderlist()
+        })
+        }
 
 
 
@@ -419,7 +425,7 @@ export const Studio_Order = () => {
 
                                             
                                             
-                                            <td> <button  className="btn btn-danger"> <i className="now-ui-icons ui-1_simple-remove"></i></button>
+                                            <td> <button  className="btn btn-danger" onClick={()=>Deletedlist(each.id)}> <i className="now-ui-icons ui-1_simple-remove"></i></button>
                                             </td>
 
 
